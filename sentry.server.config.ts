59db5ad73d5ca636b677941531d8b52af9b73ad9
None denominational guy <kylemacklane@gmail.com>
2024-07-16 18:26:43 +0300

    disable ts for the time being

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://3d627de24f5d06a1fc39000a06ca9a94@o4506813739368448.ingest.us.sentry.io/4507458386526208",
+  dsn: "https://d820444f6cd973a8e13c1d5d7bdb295d@o4507611758657536.ingest.de.sentry.io/4507611761934416",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
@@ -15,4 +15,5 @@ Sentry.init({
 
   // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
+  
 });
